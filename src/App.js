9337bbe0a7692d9bd37c4ad6d0d352b390f30be6
Daniil <daniilfgs@gmail.com>
2021-03-01 00:20:23 +0400

    Добавил Axios очень крутая штука , добавил propTypes, разработал PizzaBLock , также интересная вещь с includes.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Header} from "./components";
 import Home from "./Pages/Home";
 import {Route} from 'react-router-dom'
 import Cart from "./Pages/Cart";
+import axios from "axios";
 
 function App() {
+    const [pizzas, setPizzas] = useState([])
+    useEffect(() => {
+        axios.get("http://localhost:3000/db.json").then(({data}) => {
+            setPizzas(data.pizzas)
+        })
+        axios.post("http://localhost:3000/db.json",{data:'text Data'})
+    }, [])
 
 
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
-             <Route path = "/" exact  component = {Home}/>
-                <Route path = "/cart" exact  component = {Cart}/>
+                <Route path="/" exact render={() => <Home items={pizzas}/>}/>
+                <Route path="/cart" exact component={Cart}/>
             </div>
         </div>
     );
